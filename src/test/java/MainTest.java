d60a977fe4f650fff579321f8b2ded3a6529844a
Michael Kamen <drivesafely@gmail.com>
2021-10-11 13:55:22 -0700

    MavenProject2021Basics initial remote repository commit

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,37 @@
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainTest {
+
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "/Applications/ChromeDriver/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+            public void testSearches() {
+
+
+        driver.get("http://automationpractice.com");
+        WebElement input = driver.findElement(By.id("search_query_top"));
+        input.sendKeys("dress\n");
+
+        WebElement result = driver.findElement(By.className("lighter"));
+
+        Assert.assertEquals(result.getText(), "\"DRESS\"");
+
+    }
+    @AfterMethod
+    public void setDown(){
+        driver.quit();
+    }
+}
